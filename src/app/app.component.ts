cb404b15f7b6571bc09d98f7fe6fe0388bae37e7
Mason Hirst <mason.j.hirst@gmail.com>
2023-08-24 14:10:33 -0600

    finished the placeholder page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'par68-static-site';
+  title = 'Par 68 Golf';
+
+  copyText(text: string) {
+    navigator.clipboard.writeText(text);
+    alert('Copied to clipboard!');
+  }
 }
